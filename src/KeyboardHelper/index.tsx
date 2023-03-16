489a359ae8f292786dbf2d120fa30f5d32d1897b
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2023-03-16 12:41:08 +0100

    updated to styles and blocks

--- src/KeyboardHelper/index.tsx
+++ src/KeyboardHelper/index.tsx
@@ -3,6 +3,22 @@ import { useContext, useEffect, useState } from 'react';
 import { ShortcutObject } from '../index.types';
 import DataProvider, { DataContextInterface } from '../Store';
 
+/**
+ * @name Shortcut
+ *
+ * @description Displays the shortcut for a given shortcut ID.
+ * If the shortcut ID is not found, nothing is displayed.
+ *
+ * @param {string} shortcutId - The ID of the shortcut to display
+ * @param {boolean} asChip - Whether to display the shortcut as a chip or not
+ * @param {boolean} hasTooltip - Whether to display a tooltip with a tooltip on hover
+ *
+ * @example
+ * <Shortcut shortcutId="##openPanel##" />
+ *
+ * @returns {JSX.Element} Shortcut
+ */
+
 export default ({
   shortcutId,
   asChip = false,
