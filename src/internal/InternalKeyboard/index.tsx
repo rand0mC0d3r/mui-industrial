f79e6f676adfdfe6b614fb88cf3175f10f4a0abd
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2023-03-16 14:09:45 +0100

    stabilized keyboard triggers

--- src/internal/InternalKeyboard/index.tsx
+++ src/internal/InternalKeyboard/index.tsx
@@ -9,17 +9,17 @@ export default (): JSX.Element => {
   const handleUserKeyPress = useCallback(event => {
     const { keyCode, shiftKey, metaKey, ctrlKey, altKey } = event;
 
-    const result = shortcuts.find(shortcut => shortcut.ascii === keyCode || shortcut.char === (keyCode === 32 ? 'Space' : String.fromCharCode(keyCode)));
-    if (!result || !result.onTrigger) return;
+    const result = shortcuts
+      .find(({ ascii, char }) => ascii === keyCode || char === (keyCode === 32 ? 'Space' : String.fromCharCode(keyCode)));
+
+    if (!result || !result?.onTrigger) return;
 
     if (!!result?.altKey && !altKey) return;
     if (!!result?.ctrlKey && !ctrlKey) return;
     if (!!result?.metaKey && !metaKey) return;
     if (!!result?.shiftKey && !shiftKey) return;
 
-    console.log('triggering', result.id);
     handleCallKeyboard({ id: result.id });
-    // result.onTrigger()
   }, [shortcuts, handleCallKeyboard]);
 
   useEffect(() => {
