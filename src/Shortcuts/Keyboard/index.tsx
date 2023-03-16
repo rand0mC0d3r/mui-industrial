ceb36d4d17811dcfa2eb4eb30bcd233f697ca1ef
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2023-03-16 16:26:24 +0100

    stabilized keyboard triggers WIP

--- src/Shortcuts/Keyboard/index.tsx
+++ src/Shortcuts/Keyboard/index.tsx
@@ -8,6 +8,7 @@ export default ({
   ascii, char, label, insensitive = true, onTrigger = () => {},
 } : ShortcutObject): JSX.Element => {
   const [shortcutItem, setShortcutItem] = useState<ShortcutObject | undefined>();
+  const [announced, setAnnounced] = useState(false);
   const { shortcuts } : { shortcuts: ShortcutObject[] } = useContext(DataProvider) as DataContextInterface;
   const handleKeyboardAnnouncement = useContext(DataProvider).handleKeyboardAnnouncement as DataContextInterface['handleKeyboardAnnouncement'];
   // const { shortcuts } : { shortcuts: ShortcutObject[] } = useContext(DataProvider) as DataContextInterface;
@@ -18,29 +19,43 @@ export default ({
   //   ascii, char, label, insensitive = true, onTrigger = () => {},
   // } = props;
 
-  const callback = useCallback(() => {
-    handleKeyboardAnnouncement({ char, id, ascii, label, shiftKey, ctrlKey, altKey, metaKey, onTrigger, insensitive } as ShortcutObject);
-  }, [handleKeyboardAnnouncement, char, id, ascii, label, shiftKey, ctrlKey, altKey, metaKey, insensitive, onTrigger]);
+  // const callback = useCallback(() => {
+  //   handleKeyboardAnnouncement({ char, id, ascii, label, shiftKey, ctrlKey, altKey, metaKey, onTrigger, insensitive } as ShortcutObject);
+  // }, [handleKeyboardAnnouncement, char, id, ascii, label, shiftKey, ctrlKey, altKey, metaKey, insensitive, onTrigger]);
 
+
+  // useEffect(() => {
+  //   console.log('useEffect-s', char, id, ascii);
+  // }, [char, id, ascii]);
+
+  // useEffect(() => {
+  //   console.log('useEffect', char, id, ascii);
+  //   if ((char || ascii) && id) {
+  //     callback();
+  //   }
+  // }, [char, id, ascii, callback]);
 
   useEffect(() => {
-    console.log('useEffect', char, id, ascii);
-    if ((char || ascii) && id) {
-      callback();
+    if ((char || ascii) && id && !announced) {
+      console.log('useEffect - ann', char, id, ascii);
+      handleKeyboardAnnouncement({ char, id, ascii, label, shiftKey, ctrlKey, altKey, metaKey, onTrigger, insensitive } as ShortcutObject);
+      setAnnounced(true);
     }
-  }, [char, id, ascii, callback]);
+  }, [char, id, ascii, label, shiftKey, ctrlKey, altKey, metaKey, insensitive, onTrigger, announced, handleKeyboardAnnouncement]);
 
   useEffect(() => {
+    if (!announced) return;
     console.log('found', shortcuts.find(shortcut => shortcut.id === id));
     setShortcutItem(shortcuts.find(shortcut => shortcut.id === id));
-  }, [shortcuts, id]);
+  }, [shortcuts, announced, id]);
 
 
   useEffect(() => {
-    if (!shortcutItem) return;
+    if (!shortcutItem || !announced) return;
     console.log('triggering', shortcutItem);
+    // onTrigger()
     shortcutItem.onTrigger = onTrigger;
-  }, [shortcutItem, onTrigger]);
+  }, [shortcutItem, onTrigger, announced]);
 
   return <></>;
 };
