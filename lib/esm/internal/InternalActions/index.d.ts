a5fec50222bd80303f62aa409fd2db3d311174d0
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2023-03-27 11:18:44 +0200

    updated console and refactored header to extracted actions

--- lib/esm/internal/InternalActions/index.d.ts
+++ lib/esm/internal/InternalActions/index.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="react" />
 import { PopoverActions } from '../../index.types';
-declare const _default: ({ actions, fontSize }: {
+declare const _default: ({ actions, placement, fontSize, }: {
     actions?: PopoverActions | undefined;
+    placement?: "left" | "top" | "bottom" | "right" | "top-start" | "top-end" | "bottom-start" | "bottom-end" | "right-start" | "right-end" | "left-start" | "left-end" | undefined;
     fontSize: string;
 }) => JSX.Element;
 export default _default;
